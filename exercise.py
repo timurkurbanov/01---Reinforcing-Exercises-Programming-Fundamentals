fccf4fc16ac6904b5111d73d5a6bb6573aacf6b0
TIMUR <1tkurbanov@gmail.com>
2019-05-10 16:09:20 -0400

    commit

---
+++ exercise.py
@@ -0,0 +1,18 @@
+venues = [
+{ 'address': "123 Main Street", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 100 },
+{ 'address': "567 Centre Street", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 400 },
+{ 'address': "9B Ontario Street", 'city': "Montreal", 'wheelchair_accessible': True, 'capacity': 1000 },
+{ 'address': "56 Road Avenue", 'city': "Ottawa", 'wheelchair_accessible': True, 'capacity': 650 },
+{ 'address': "938 Avenue Way East", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 90 },
+{ 'address': "34 Main Street West", 'city': "London", 'wheelchair_accessible': False, 'capacity': 300 },
+{ 'address': "44 Quebec Road", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 200 },
+{ 'address': "10 Spruce Avenue Ouest", 'city': "Montreal", 'wheelchair_accessible': False, 'capacity': 525 }
+
+]
+
+new_venues = []
+for venue in venues:
+    if venue['wheelchair_accessible'] == True and venue['city'] == 'Toronto' and venue['capacity'] >= 150:
+        new_venues.append(venue)
+
+print(new_venues)
